476888c963e38f89d280956cb2b2fb6d8b190252
gitaryaman <apan45@student.monash.edu>
2016-05-27 15:37:15 +1000

    Main page + view location work in conjunction
    Slider functionality still needs to be fixed

--- code/js/mainPage.js
+++ code/js/mainPage.js
@@ -4,6 +4,19 @@ contentRef = document.getElementById("main");
 // Allows time for previous data to be stored in LocalStorage
 initialise();
 
+function whatWasClicked(event)
+{
+    var className = event.srcElement.className;
+    locationClicked(classNameToLocationIndex(className));
+}
+
+// Parses the class name to return a location index
+function classNameToLocationIndex(classNameString)
+{
+    var arrayOfWords = classNameString.split(" ");
+    var locationIndex = arrayOfWords[1];
+    return Number(locationIndex);
+}
 
 // Adds a location summary node to the content div
 function addLocationSummary(index, response)
@@ -19,47 +32,48 @@ function addLocationSummary(index, response)
     var iconRef = document.createElement("img");
     var iconSource = "images/" + dailyData.icon + ".png";
     console.log(iconSource);
-    iconRef.setAttribute("class", "icon");
+    iconRef.setAttribute("class", "icon" + " " + String(index));
     iconRef.setAttribute("src", iconSource);
     
     // Creating the <div> tag with class="icon-holder"
     var iconHolderRef = document.createElement("div");
-    iconHolderRef.setAttribute("class", "icon-holder");
+    iconHolderRef.setAttribute("class", "icon-holder" + " " + String(index));
     iconHolderRef.appendChild(iconRef);
     
     // Creating the <div> tag with class="temp-low"
     var tempLowRef = document.createElement("div");
-    tempLowRef.setAttribute("class", "temp-low");
+    tempLowRef.setAttribute("class", "temp-low" + " " + String(index));
     tempLowRef.innerHTML = Math.round(dailyData.temperatureMin);
     
     // Creating the <div> tag with class="temp-high"
     var tempHighRef = document.createElement("div");
-    tempHighRef.setAttribute("class", "temp-high");
+    tempHighRef.setAttribute("class", "temp-high" + " " + String(index));
     tempHighRef.innerHTML = Math.round(dailyData.temperatureMax);
     
     // Creating the <div> tag with class="top"
     var topRef = document.createElement("div");
-    topRef.setAttribute("class", "top");
+    topRef.setAttribute("class", "top" + " " + String(index));
     topRef.appendChild(tempLowRef);
     topRef.appendChild(iconHolderRef);
     topRef.appendChild(tempHighRef);
     
     // Creating the <div> tag with class="bottom"
-    var bottomRef = document.createElement("div");
-    bottomRef.setAttribute("class", "bottom");
+    var bottomRef = document.createElement("div" );
+    bottomRef.setAttribute("class", "bottom" + " " + String(index));
     bottomRef.innerHTML = currentLocation.nickname;
     
     // Creating the <div> tag with class="location-summary"
     var locationSummaryRef = document.createElement("div");
-    locationSummaryRef.setAttribute("class", "location-summary");
+    locationSummaryRef.setAttribute("class", "location-summary" + " " + String(index));
     locationSummaryRef.appendChild(topRef);
     locationSummaryRef.appendChild(bottomRef);
 
     // Creating the <a> tag
-    var locationLinkRef = document.createElement("a");
-    locationLinkRef.setAttribute("class", "location-link");
-    locationLinkRef.setAttribute("href", "viewlocation.html");
+    var locationLinkRef = document.createElement("div");
+    locationLinkRef.setAttribute("class", "location-link" + " " + String(index));
+    console.log(this);
     locationLinkRef.appendChild(locationSummaryRef);
+    locationLinkRef.addEventListener("click", whatWasClicked, false);
     
     // Linking everything back to the main content
     contentRef.appendChild(locationLinkRef);
@@ -79,4 +93,31 @@ function initialise()
             
             locationWeatherCache.getWeatherAtIndexForDate(locationIndex, currentDate, addLocationSummary);
         }
+}
+
+// Save the last location clicked to Local Storage.
+function locationClicked(locationIndex) {
+    console.log("clicked");
+    if (typeof locationIndex === "number")
+        {
+            var locationIndexObject = {index: locationIndex};
+
+            // lio = locationIndexObject
+            var lioJSON = JSON.stringify(locationIndexObject);
+            localStorage.setItem("location-clicked", lioJSON);
+            
+            window.location.href = "viewlocation.html";
+        }
+}
+
+function saveLocationClicked(locationIndex)
+{
+    var lioJSON = localStorage.getItem("location-clicked");
+    
+    // Check if app data is present in local storage
+    if (lioJSON)
+    {
+        var lioPDO = JSON.parse(lioJSON);
+        locationWeatherCache.initialiseFromPDO(lioPDO);
+    }
 }
--- code/js/viewLocationPage.js
+++ code/js/viewLocationPage.js
@@ -1,12 +1,122 @@
 // Code for the View Location page.
 
-// This is sample code to demonstrate navigation.
-// You need not use it for final app.
+//Global Variables
+var selectedLocation;
+var locationIndex;
+var map;
+var mapRef;
+var geocoder;
+var infoWindow;
+var dateToDisplay;
 
-var locationIndex = localStorage.getItem(APP_PREFIX + "-selectedLocation"); 
-if (locationIndex !== null)
+var lioJSON = localStorage.getItem("location-clicked");
+
+
+window.addEventListener("load", pageFullyLoaded, false);
+    
+// Check if app data is present in local storage
+if (lioJSON)
 {
-    var locationNames = [ "Location A", "Location B" ];
-    // If a location name was specified, use it for header bar title.
-    document.getElementById("headerBarTitle").textContent = locationNames[locationIndex];
+    var locationIndexObject = JSON.parse(lioJSON);
+    locationIndex = locationIndexObject.index;
+    selectedLocation = locationWeatherCache.locationAtIndex(locationIndex);
+}
+
+// Navigation
+
+if (selectedLocation !== undefined) {
+    document.getElementById("headerBarTitle").textContent = selectedLocation.nickname;
+}
+
+///////////////////////////////////////////////////////////MAP/////////////////////////////////////////////////////////////////////////////////
+
+function pageFullyLoaded(e)
+{   
+    mapRef = document.getElementById('map');
+    initMap();
+}
+
+function initMap()
+{
+    var latitude = Number(selectedLocation.latitude);
+    var longitude = Number(selectedLocation.longitude);
+    
+    var position = {lat: latitude, lng: longitude};
+    
+    var mapOptions = {
+        center: position,
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    
+    map = new google.maps.Map(mapRef, mapOptions);
+    
+    infowindow = new google.maps.InfoWindow;
+
+    // Display an overlay with a location pin and label.
+    
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+    
+    infowindow.setContent(selectedLocation.nickname);
+    infowindow.open(map, marker);
+    
+    geocoder = new google.maps.Geocoder;
+    
+    initWeatherInfo(30);
+}
+
+function initWeatherInfo(sliderValue)
+{
+    dateToDisplay = sliderDate(sliderValue);
+    locationWeatherCache.getWeatherAtIndexForDate(locationIndex, dateToDisplay, updateDOM);
+}
+
+////////////////////////////////////////////////////////////DATES////////////////////////////////////////////////////////////////////////////////
+
+//generates a date based on the position of the slider, called as part of the slider callback function and forecastAPI function
+function sliderDate(value) {
+    var date = new Date();
+    var msecSince1970 = date.getTime();
+    var numberOfDaysBefore = 30 - value;
+    msecSince1970 -= numberOfDaysBefore * 86400000;
+    var sliderDate = new Date(msecSince1970);
+    return sliderDate;
+}
+
+//called as a callback function with slider. Generates a string date and displays in the "date" field in the DOM
+function sliderCallback(value) {
+    var selectedDate = sliderDate(value);
+    var stringDate = selectedDate.simpleDateString();
+    document.getElementById("date").innerHTML = "Date: " + stringDate;
+}
+
+//called by the forecast.io call. Generates a date readable by forecast.io
+function forecastAPIDate(value) {
+    var forecastDate = sliderDate(value);
+    return forecastDate.forecastDateString();
+}
+
+///////////////////////////////////////////////////////WEATHER INFORMATION///////////////////////////////////////////////////////////////////////
+
+
+//creates a DOM instance with all information added to the html file
+function updateDOM(locationIndex, forecastData){
+    
+    var dailyData = forecastData.daily.data[0];
+    // variables
+    document.getElementById("currentConditions").innerHTML = dailyData.summary;
+    document.getElementById("maxTemp").innerHTML = Math.round(dailyData.temperatureMax) + "&#8451";
+    document.getElementById("minTemp").innerHTML = Math.round(dailyData.temperatureMin) + "&#8451";
+    document.getElementById("humidity").innerHTML = Math.round(dailyData.humidity) + "%";
+    document.getElementById("windSpeed").innerHTML = Math.round(dailyData.windSpeed) + "km/h";
+}
+
+///////////////////////////////////////////////////////REMOVE LOCATION BUTTON///////////////////////////////////////////////////////////////////?
+
+function removeLocationButton(locationIndex){
+    locationWeatherCache.removeLocationAtIndex(locationIndex);
+    LocationWeatherCache.saveLocations();
 }
